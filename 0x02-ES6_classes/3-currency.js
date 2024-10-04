e05099a1704f2d2ffbdc4a152961010334045f6c
chisonia <sonya12slyvia@gmail.com>
2024-10-04 11:17:23 +0100

    creating method

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,31 @@
+export default class Currency{
+    constructor(code, name){
+        this.code = code;
+        this.name = name;
+    }
+
+    get code (){
+        this._code = code;
+    }
+    get name (){
+        this._name = name;
+    }
+
+    set code(value){
+        if (typeof value !== 'string') {
+            throw new TypeError('Code must be a string');
+          }
+        this._code = value;
+    }
+    set name(value){
+        if (typeof value !== 'string') {
+            throw new TypeError('Name must be a string');
+          }
+        this._name = value
+    }
+
+    displayFullCurrency(){
+        return `${this._name}(${this._code})`;
+    }
+}
+
