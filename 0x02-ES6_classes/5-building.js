fda3e2b6a9260b35774af1484916ded6fedef45f
chisonia <sonya12slyvia@gmail.com>
2024-10-07 17:00:21 +0100

    New correction

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,16 +1,16 @@
 export default class Building {
-    constructor(sqft) {
-        if (new.target === Building) {
-            throw new TypeError('Cannot construct Building instances directly');
-        }
-        this._sqft = sqft;
+  constructor(sqft) {
+    if (new.target === Building) {
+      throw new TypeError('Cannot construct Building instances directly');
     }
+    this._sqft = sqft;
+  }
 
-    get sqft() {
-        return this._sqft;
-    }
+  get sqft() {
+    return this._sqft;
+  }
 
-    evacuationWarningMessage() {
-        throw new Error('Class extending Building must override evacuationWarningMessage');
-    }
+  evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
 }
