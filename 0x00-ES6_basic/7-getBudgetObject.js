fae6a12136c682969da356896e775a9081604d13
chisonia <sonya12slyvia@gmail.com>
2024-10-07 15:02:09 +0100

    new

--- 0x00-ES6_basic/7-getBudgetObject.js
+++ 0x00-ES6_basic/7-getBudgetObject.js
@@ -1,4 +1,4 @@
 export default function getBudgetObject(income, gdp, capita) {
-  const budget = { income, gdp, capita };
+  const budget = { income, gdp, capita };// ES6 shorthand for { income: income, gdp: gdp, capita: capita }
   return budget;
 }
