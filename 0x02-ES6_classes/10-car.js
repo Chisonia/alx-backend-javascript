8221b59d6f43b6366e8589ae4f89f8c4ff3565f5
chisonia <sonya12slyvia@gmail.com>
2024-10-08 10:58:14 +0100

    correct 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,35 +1,15 @@
+const _brand = Symbol('brand'); // Define a symbol for brand
+const _motor = Symbol('motor'); // Define a symbol for motor
+const _color = Symbol('color'); // Define a symbol for color
+
 export default class Car {
   constructor(brand, motor, color) {
-    this._brand = brand; // Store as regular properties with an underscore prefix
-    this._motor = motor;
-    this._color = color;
+    this[_brand] = brand; // Store brand using symbol
+    this[_motor] = motor; // Store motor using symbol
+    this[_color] = color; // Store color using symbol
   }
 
-  set brand(value) {
-    this._brand = value;
-  }
-
-  set motor(value) {
-    this._motor = value;
-  }
-
-  set color(value) {
-    this._color = value;
-  }
-
-  get brand() {
-    return this._brand;
-  }
-  
-  get motor() {
-    return this._motor;
-  }
-  
-  get color() {
-    return this._color;
-  }
-  
   cloneCar() {
-    return new this.constructor();
+    return new this.constructor(); // Return a new instance of the class
   }
 }
