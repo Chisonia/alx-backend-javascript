a28f0fac9442a957a85bbb13783e26cceaedc17c
chisonia <sonya12slyvia@gmail.com>
2024-10-08 10:54:34 +0100

    new

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -4,6 +4,31 @@ export default class Car {
     this._motor = motor;
     this._color = color;
   }
+
+  set brand(value) {
+    this._brand = value;
+  }
+
+  set motor(value) {
+    this._motor = value;
+  }
+
+  set color(value) {
+    this._color = value;
+  }
+
+  get brand() {
+    return this._brand;
+  }
+  
+  get motor() {
+    return this._motor;
+  }
+  
+  get color() {
+    return this._color;
+  }
+  
   cloneCar() {
     return new this.constructor();
   }
