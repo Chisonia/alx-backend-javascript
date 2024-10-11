ba977415ba2b22601e4ca4e2e7609e56fb99bcf5
chisonia <sonya12slyvia@gmail.com>
2024-10-11 11:53:57 +0100

    update

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,12 @@
+/* eslint-disable no-console */
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((userDetails) => {
-      const [userPhoto, userName] = userDetails;
-      console.log(`${userPhoto.body} ${userName.firstName}, ${userName.lastName}`);
-    })
-    .catch(() => { console.log('Signup system offline'); });
+  try{const photo = uploadPhoto();
+  const user = createUser();
+  return Promise.all([photo, user]).then((userDetails) => {
+    const [userPhoto, userName] = userDetails;
+    console.log(`${userPhoto.body} ${userName.firstName}, ${userName.lastName}`);
+  })}
+    catch{ () => { console.log('Signup system offline'); } };
 }
