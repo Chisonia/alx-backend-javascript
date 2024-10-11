3ff87be9fdefe25d9db7c16fb782455bfc5b7241
chisonia <sonya12slyvia@gmail.com>
2024-10-11 14:13:40 +0100

    correction

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,12 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, filename) {
-  const signUser = signUpUser(firstName, lastName);
-  const photoUser = uploadPhoto(filename);
-  const details = await Promise.allSettled([signUser, photoUser]);
+  // Wait for all promises to settle
+  const details = await Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(filename),
+  ]);
+
   const result = [];
   details.forEach((item) => {
     if (item.status === 'fulfilled') {
@@ -13,5 +16,6 @@ export default async function handleProfileSignup(firstName, lastName, filename)
       result.push({ status: item.status, value: `${item.reason}` });
     }
   });
+
   return result;
 }
