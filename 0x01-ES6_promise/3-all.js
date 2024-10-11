05ecf7d71179aac06c4be48f98f7d77fed5a6bf6
chisonia <sonya12slyvia@gmail.com>
2024-10-11 10:09:22 +0100

    Promise.all correction

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((userDetails) => {
-      const [userPhoto, userName] = userDetails;
-      console.log(`${userPhoto.body} ${userName.firstName}, ${userName.lastName}`);
-    })
-    .catch(() => { console.log('Signup system offline'); });
+export default async function handleProfileSignup() {
+  try {
+        const userDetails = await Promise.all([uploadPhoto(), createUser()]);
+        const [userPhoto, userName] = userDetails;
+        console.log(`${userPhoto.body} ${userName.firstName}, ${userName.lastName}`);
+    } catch {
+        console.log('Signup system offline');
+    }
 }
