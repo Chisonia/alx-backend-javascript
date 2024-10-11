2c308faac267b8313c045cfc982de9125e890861
chisonia <sonya12slyvia@gmail.com>
2024-10-11 14:19:53 +0100

    new

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,16 +1,12 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-  // Execute the promises
-  const results = await Promise.allSettled([
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]);
-
-  // Format the result to match the required structure
-  return results.map((result) => ({
+    uploadPhoto(filename),
+  ]).then((results) => results.map((result) => ({
     status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  }));
+    value: result.status === 'fulfilled' ? result.value : String(result.reason),
+  })));
 }
