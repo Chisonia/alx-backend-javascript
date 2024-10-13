43877a3f99ac5bb187aca4f4888558f8e3dbadbd
chisonia <sonya12slyvia@gmail.com>
2024-10-13 15:14:40 +0100

    update

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,10 +1,10 @@
-export default function guardrail (mathFunction) {
+export default function guardrail(mathFunction) {
   const quene = [];
   try{
     const result = mathFunction();
     quene.push(result);
   } catch (error) {
-        quene.push(`Error: ${ error.message }`);
+    quene.push(`Error: ${ error.message }`);
   } finally { quene.push('Guardrail was processed') }
   return quene;
 }
